c91cbea9d53e15bcaec310b053eb246bdf147269
Sumikesumikesu <135323426+Sumikesumikesu@users.noreply.github.com>
2025-01-31 20:50:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ from gigachat_module import get_gigachat_answer
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-id = os.getenv('ID')
 
-rss_link = "https://sports.yahoo.com/nba/rss.xml"
-news = []
+def main() -> None:
+    load_dotenv()
+    id = os.getenv('ID')
+    
+    rss_link = "https://sports.yahoo.com/nba/rss.xml"
+    news = []
+    
+    with open("post_instruction.txt", "r") as f:
+        instruction = f.read()
+    
+    news = parse_news(rss_link)
+    result = get_gigachat_answer(id, news, instruction)
+    
+    with open('digest.txt', 'w') as f:
+        f.write(result)
+    
+    print(result)
 
-with open("post_instruction.txt", "r") as f:
-    instruction = f.read()
 
-news = parse_news(rss_link)
-result = get_gigachat_answer(id, news, instruction)
-
-with open('digest.txt', 'w') as f:
-    f.write(result)
-
-print(result)
+if __name__ == '__main__':
+    main()
